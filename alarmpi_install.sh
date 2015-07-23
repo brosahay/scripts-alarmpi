09004c9f5af921fcecd19c39f82f6346d1a227f0
revosftw <sahay.rohan@gmail.com>
2015-07-23 18:55:16 +0530

    adding config files

--- alarmpi_install.sh
+++ alarmpi_install.sh
@@ -6,16 +6,17 @@
 
 #### VARIABLES ####
 device="/dev/mmcblk0"
-partitions=($(lsblk "/dev/$device" | fgrep '─' | sed -E 's/^.+─(\w+).+$/\1/g'))
 ###################
 
 (echo o; echo n; echo p;echo 1; echo ; echo +100M; echo t; echo c; echo n; echo p; echo 2; echo ; echo ;echo w)|sudo fdisk $device
-sudo mkfs.vfat "${device}${partitions[0]}"
+sudo fdisk -l $device
+partitions=($(lsblk "$device" | fgrep '─' | sed -E 's/^.+─(\w+).+$/\1/g'))
+sudo mkfs.vfat "/dev/${partitions[0]}"
 mkdir boot
-sudo mount "${device}${partitions[0]}" boot
-sudo mkfs.ext4 "${device}${partitions[1]}"
+sudo mount "/dev/${partitions[0]}" boot
+sudo mkfs.ext4 "/dev/${partitions[1]}"
 mkdir root
-sudo mount "${device}${partitions[1]}" root
+sudo mount "/dev/${partitions[1]}" root
 if ls|grep -qs 'ArchLinuxARM-rpi'; then
 	echo "Using old copy of ArchLinuxARM"
 else
@@ -25,5 +26,5 @@ sudo su -c 'bsdtar -xpf ArchLinuxARM-rpi-latest.tar.gz -C root'
 sudo su -c 'sync'
 sudo mv root/boot/* boot 
 sudo umount boot root
-rm -rf boot root alarmpi
+sudo su -c "rm -rf boot root"
 echo "SD CARD READY with ArchLinuxARM"
