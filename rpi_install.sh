38ac73d83660846f06acb2fe1f10d4993b1f36ea
revosftw <sahay.rohan@gmail.com>
2015-09-27 14:19:12 +0530

    skipping reinstalling of pacman packages

--- rpi_install.sh
+++ rpi_install.sh
@@ -3,7 +3,7 @@
 #### 	23- Jul - 2015 		####
 
 #### VARIABLES ####
-default_user="pi"
+default_user="alarm"
 torrent_path="/mnt/share"
 drive_path="/dev/sda2"
 
@@ -13,22 +13,26 @@ drive_path="/dev/sda2"
 #### CHANGE ROOT PASSWORD ####
 passwd
 
-#### PACMAN ####
-sed -i 's/#Color/Color/' /etc/pacman.conf
-
 #### UPDATE rPi AND INSTALL PACKAGES ####
 pacman -Syu --noconfirm
+pacman -S wget --noconfirm --needed
+
+#### PACMAN ####
+sed -i 's/#Color/Color/' /etc/pacman.conf
+sed -i 's/#XferCommand = \/usr\/bin\/wget --passive-ftp -c -O %o %u/XferCommand = \/usr\/bin\/wget --passive-ftp -c -O -q --show-progress \x27%o\x27 \x27%u\x27/' /etc/pacman.conf
+
+#### UPDATE rPi AND INSTALL PACKAGES ####
 #pacman -S polkit --noconfirm
-pacman -S base-devel vim zsh wget libnewt --noconfirm
-pacman -S python2 python2-pip python2-lxml --noconfirm
+pacman -S base-devel vim zsh wget libnewt --noconfirm --needed
+pacman -S python2 python2-pip python2-lxml --noconfirm --needed
 pip2 install mitmproxy
-pacman -S filesystem nfs-utils autofs diffutils --noconfirm
+pacman -S filesystem nfs-utils autofs diffutils --noconfirm --needed
 #AUDIO
 #pacman -S alsa-utils alsa-firmware alsa-lib alsa-plugins
 #Wi-Fi
-pacman -S dialog wpa_supplicant --noconfirm
+pacman -S dialog wpa_supplicant --noconfirm --needed
 #OPTIONAL
-pacman -S packer ntp transmission-cli git htop ntfs-3g --noconfirm
+pacman -S packer ntp transmission-cli git htop ntfs-3g --noconfirm --needed
 
 #### ADD A NEW USER AS pi ####
 useradd -m -g users -G wheel -s /bin/zsh $default_user
