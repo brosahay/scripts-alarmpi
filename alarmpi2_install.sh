ea4e070548754ff771d5a2b3cb809ebbb51e5b91
revostfw <sahay.rohan@gmail.com>
2016-04-05 11:29:04 +0530

    added FS Labels and BSDTAR verbose

--- alarmpi2_install.sh
+++ alarmpi2_install.sh
@@ -5,17 +5,17 @@
 #!/bin/bash
 
 #### VARIABLES ####
-device="/dev/mmcblk0"
+device="/dev/sdd"
 partitions=($(lsblk "$device" | fgrep '─' | sed -E 's/^.+─(\w+).+$/\1/g'))
 ###################
 sudo umount "/dev/${partitions[0]}"
 sudo umount "/dev/${partitions[1]}"
 (echo o; echo n; echo p;echo 1; echo ; echo +100M; echo t; echo c; echo n; echo p; echo 2; echo ; echo ;echo w)|sudo fdisk $device
 sudo fdisk -l $device
-sudo mkfs.vfat "/dev/${partitions[0]}"
+sudo mkfs.vfat -n "arm_boot" "/dev/${partitions[0]}"
 mkdir boot
 sudo mount "/dev/${partitions[0]}" boot
-sudo mkfs.ext4 "/dev/${partitions[1]}"
+sudo mkfs.ext4 -L "arm_root" "/dev/${partitions[1]}"
 mkdir root
 sudo mount "/dev/${partitions[1]}" root
 if ls|grep -qs 'ArchLinuxARM-rpi-2'; then
@@ -23,7 +23,7 @@ if ls|grep -qs 'ArchLinuxARM-rpi-2'; then
 else
 	wget -q --show-progress -c http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
 fi
-sudo su -c 'bsdtar -xpf ArchLinuxARM-rpi-2-latest.tar.gz -C root'
+sudo su -c 'bsdtar -xvpf ArchLinuxARM-rpi-2-latest.tar.gz -C root'
 sudo su -c 'sync'
 sudo mv root/boot/* boot 
 sudo umount boot root
